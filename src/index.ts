8a2c356af40514b64e6fbf65d7be2cc51d93ac29
John McGarvey <johnmcgarvey55@gmail.com>
2023-08-25 15:47:34 -0400

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,87 @@
-import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon } from '@nodegui/nodegui';
+//import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon } from '@nodegui/nodegui';
 import logo from '../assets/logox200.png';
 
-const win = new QMainWindow();
-win.setWindowTitle("Hello World");
+// const win = new QMainWindow();
+// win.setWindowTitle("Hello World");
 
-const centralWidget = new QWidget();
-centralWidget.setObjectName("myroot");
-const rootLayout = new FlexLayout();
-centralWidget.setLayout(rootLayout);
+// const centralWidget = new QWidget();
+// centralWidget.setObjectName("myroot");
+// const rootLayout = new FlexLayout();
+// centralWidget.setLayout(rootLayout);
 
-const label = new QLabel();
-label.setObjectName("mylabel");
-label.setText("Hello");
+// const label = new QLabel();
+// label.setObjectName("mylabel");
+// label.setText("Hello");
 
-const button = new QPushButton();
-button.setIcon(new QIcon(logo));
+// const button = new QPushButton();
+// button.setIcon(new QIcon(logo));
 
-const label2 = new QLabel();
-label2.setText("World");
-label2.setInlineStyle(`
-  color: red;
-`);
+// const label2 = new QLabel();
+// label2.setText("World");
+// label2.setInlineStyle(`
+//   color: red;
+// `);
 
-rootLayout.addWidget(label);
-rootLayout.addWidget(button);
-rootLayout.addWidget(label2);
-win.setCentralWidget(centralWidget);
-win.setStyleSheet(
-  `
-    #myroot {
-      background-color: #009688;
-      height: '100%';
-      align-items: 'center';
-      justify-content: 'center';
+// rootLayout.addWidget(label);
+// rootLayout.addWidget(button);
+// rootLayout.addWidget(label2);
+// win.setCentralWidget(centralWidget);
+// win.setStyleSheet(
+//   `
+//     #myroot {
+//       background-color: #009688;
+//       height: '100%';
+//       align-items: 'center';
+//       justify-content: 'center';
+//     }
+//     #mylabel {
+//       font-size: 16px;
+//       font-weight: bold;
+//       padding: 1;
+//     }
+//   `
+// );
+// win.show();
+
+// (global as any).win = win;
+import { QMessageBox, ButtonRole, QPushButton, WindowState } from '@nodegui/nodegui';
+const messageBox = new QMessageBox();
+messageBox.setText('John has invited you to the McGarvey Family Zoom meeting.')
+const zoomButton = new QPushButton();
+const cancelButton = new QPushButton();
+zoomButton.setText('Start the Zoom Meeting');
+cancelButton.setText('Cancel');
+messageBox.addButton(cancelButton,ButtonRole.RejectRole);
+messageBox.addButton(zoomButton,ButtonRole.AcceptRole);
+
+messageBox.setWindowTitle("McGarvey Family Zoom");
+const sureBox = new QMessageBox();
+const yesButton = new QPushButton();
+const noButton = new QPushButton();
+noButton.setText("No");
+yesButton.setText("Yes");
+sureBox.setWindowTitle("McGarvey Family Zoom");
+sureBox.setText("Are you sure you want to cancel and not start Zoom?");
+sureBox.addButton(yesButton,ButtonRole.AcceptRole);
+sureBox.addButton(noButton,ButtonRole.RejectRole);
+
+let decision;
+do {
+  messageBox.setModal(true);
+  messageBox.setWindowState(WindowState.WindowActive);
+  //messageBox.setDefaultButton(zoomButton);
+  decision = messageBox.exec();
+  let sure;
+  if (!decision) {
+    sureBox.setModal(true);
+    //sureBox.setDefaultButton(noButton);
+    sure = sureBox.exec();
+    console.log("sure: ", sure)
+    if (sure) {
+        break;
     }
-    #mylabel {
-      font-size: 16px;
-      font-weight: bold;
-      padding: 1;
-    }
-  `
-);
-win.show();
-
-(global as any).win = win;
+  } else {
+    break;
+  }
+} while (true);
+console.log("decision: ", decision)
